b5abf0dc5edf822ff026342e2e1195ffd32a8abf
Devil-Aura <imh203356@gmail.com>
2024-12-16 11:32:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -17,8 +17,8 @@ class Config(object):
 
     # other configs
     BOT_UPTIME = time.time()
-    START_PIC = os.environ.get("START_PIC", "https://telegra.ph/file/0ceb5f176f3cf877a08b5.jpg")
-    ADMIN = int(os.environ.get('ADMIN', ''))  # ⚠️ Required
+    START_PIC = os.environ.get("START_PIC", "https://radare.arzfun.com/api/tg/photo?id=AgACAgUAAxkBAAECqEtnX8ItNqKoU5WriPcvECFDhNOpGwACmb4xG5e--Vb0_u6TDced2QEAAwIAA3kAAzYE")
+    ADMIN = int(os.environ.get('ADMIN', '6040503076'))  # ⚠️ Required
     DEFAULT_WELCOME_MSG = os.environ.get("WELCOME_MSG", "Hey {user},\nYour Request Approved ✅,\n\nWelcome to **{title}**")
     DEFAULT_LEAVE_MSG = os.environ.get("LEAVE_MSG", "By {user},\nSee You Again 👋\n\nFrom **{title}**")
 
